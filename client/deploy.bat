1460970db2a0e2ee8c8943f44b2c42d944f549a5
cxzucc <cxzucc@163.com>
2015-02-03 00:42:08 +0800

    发布源码到仓库。

--- client/deploy.bat
+++ client/deploy.bat
@@ -1,3 +1,3 @@
-call mvn package
-call mvn deploy:deploy-file -DgroupId=com.kaisen.usercenter -DartifactId=usercenter.client -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar -Dfile=target/usercenter.client-0.0.1-SNAPSHOT.jar -Durl=http://192.168.2.210:10001/nexus/content/repositories/snapshots/ -DrepositoryId=snapshots
+call mvn clean source:jar package
+call mvn deploy:deploy-file -DgroupId=com.kaisen.usercenter -DartifactId=usercenter.client -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar -Dfile=target/usercenter.client-0.0.1-SNAPSHOT.jar -Dsources=target/usercenter.client-0.0.1-SNAPSHOT-sources.jar -Durl=http://192.168.2.210:10001/nexus/content/repositories/snapshots/ -DrepositoryId=snapshots
 @pause
